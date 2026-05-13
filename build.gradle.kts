d7cacca0c43d7463b836b08ad5e01a2a6212be26
Arctic <arcticdev@arcticgaming.dev>
2026-05-13 13:30:57 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java-library")
+    id("com.gradleup.shadow") version "9.4.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/java/dev/arctic/goob/Goob.java
@@ -0,0 +1,16 @@
+package dev.arctic.goob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Goob extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
